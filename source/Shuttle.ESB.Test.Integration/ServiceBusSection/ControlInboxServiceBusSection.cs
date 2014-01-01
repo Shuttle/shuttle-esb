dcb49abf68a6f7f57b9c40d6424a06bd90ab616a
Eben <me@ebenroux.co.za>
2014-01-01 20:22:38 +0200

    - updated local

---
+++ source/Shuttle.ESB.Test.Integration/ServiceBusSection/ControlInboxServiceBusSection.cs
@@ -0,0 +1,33 @@
+using System;
+using NUnit.Framework;
+using Shuttle.ESB.Core;
+
+namespace Shuttle.ESB.Test.Integration
+{
+    [TestFixture]
+    public class ControlInboxServiceBusSection : ServiceBusSectionFixture
+    {
+        [Test]
+        public void Should_be_able_to_load_a_full_configuration()
+        {
+            var section = GetServiceBusSection("Control-Full.config");
+
+            Assert.IsNotNull(section);
+
+			Assert.AreEqual("msmq://./control_inbox_work", section.ControlInbox.WorkQueueUri);
+			Assert.AreEqual("msmq://./control_inbox_error", section.ControlInbox.ErrorQueueUri);
+			Assert.AreEqual("msmq://./control_inbox_journal", section.ControlInbox.JournalQueueUri);
+         
+
+            Assert.AreEqual(25, section.ControlInbox.ThreadCount);
+            Assert.AreEqual(25, section.ControlInbox.MaximumFailureCount);
+
+            Assert.AreEqual(TimeSpan.FromMilliseconds(250), section.ControlInbox.DurationToSleepWhenIdle[0]);
+            Assert.AreEqual(TimeSpan.FromSeconds(10), section.ControlInbox.DurationToSleepWhenIdle[1]);
+            Assert.AreEqual(TimeSpan.FromSeconds(30), section.ControlInbox.DurationToSleepWhenIdle[2]);
+
+            Assert.AreEqual(TimeSpan.FromMinutes(30), section.ControlInbox.DurationToIgnoreOnFailure[0]);
+            Assert.AreEqual(TimeSpan.FromHours(1), section.ControlInbox.DurationToIgnoreOnFailure[1]);
+        }
+    }
+}
