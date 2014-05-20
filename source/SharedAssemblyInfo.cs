10fc3a77dc5812bb05d4895f3093203ec41b20b6
Eben <me@ebenroux.co.za>
2014-05-20 17:06:29 +0200

    - updated to version 3.0.0-rc7

--- source/SharedAssemblyInfo.cs
+++ source/SharedAssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Shuttle.ESB")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("3.0.0-rc6")]
+[assembly: AssemblyInformationalVersion("3.0.0-rc7")]
 [assembly: ComVisible(false)]
 
