f9e980856903be324917c2ea4cd2c6328f2f2802
Eben <me@ebenroux.co.za>
2022-03-15 20:20:49 +0200

    deploy

---
+++ assets/container_shuttle-core-unity.md.7ae1ef5d.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.764113ec.js";
+const __pageData = '{"title":"Shuttle.Core.Unity","description":"","frontmatter":{"title":"Shuttle.Core.Unity","layout":"api"},"headers":[],"relativePath":"container/shuttle-core-unity.md"}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="shuttle-core-unity" tabindex="-1">Shuttle.Core.Unity <a class="header-anchor" href="#shuttle-core-unity" aria-hidden="true">#</a></h1><div class="language-"><pre><code>PM&gt; Install-Package Shuttle.Core.Unity\n</code></pre></div><p>The <code>UnityComponentContainer</code> implements the <code>IComponentContainer</code> interface.</p><div class="language-c#"><pre><code><span class="line"><span style="color:#F78C6C;">var</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">container</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">new</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">UnityComponentContainer</span><span style="color:#89DDFF;">(</span><span style="color:#F78C6C;">new</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">UnityContainer</span><span style="color:#89DDFF;">());</span></span>\n<span class="line"></span></code></pre></div>', 4);
+const _hoisted_5 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_5);
+}
+var shuttleCoreUnity = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, shuttleCoreUnity as default };
