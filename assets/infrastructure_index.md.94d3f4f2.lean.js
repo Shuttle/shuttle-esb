8091ee8012e79b6e6c6c9e546bd76f69ed76546d
Eben <me@ebenroux.co.za>
2022-03-16 07:21:13 +0200

    deploy

---
+++ assets/infrastructure_index.md.94d3f4f2.lean.js
@@ -0,0 +1,24 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, b as createBaseVNode, d as createTextVNode } from "./app.bd43fc14.js";
+const __pageData = '{"title":"Infrastructure","description":"","frontmatter":{},"headers":[],"relativePath":"infrastructure/index.md"}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h1", {
+  id: "infrastructure",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("Infrastructure "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#infrastructure",
+    "aria-hidden": "true"
+  }, "#")
+], -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("p", null, "The packages under this collection do not have specific implementations.", -1);
+const _hoisted_3 = [
+  _hoisted_1,
+  _hoisted_2
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_3);
+}
+var index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, index as default };
