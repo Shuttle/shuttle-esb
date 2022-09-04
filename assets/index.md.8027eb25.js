8d36d306027cf8f155cf25a33d40a16ac3d3f635
Eben <me@ebenroux.co.za>
2022-09-04 13:23:08 +0200

    deploy

---
+++ assets/index.md.8027eb25.js
@@ -0,0 +1,24 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode, e as createVNode } from "./app.28dce079.js";
+var Home_vue_vue_type_style_index_0_scoped_true_lang = "";
+const _sfc_main$1 = {};
+const _hoisted_1 = { id: "hero" };
+const _hoisted_2 = /* @__PURE__ */ createStaticVNode('<p class="upgrade-notice" data-v-664d0f7e> The latest version of the Shuttle.Esb components now make use of <a href="https://docs.microsoft.com/en-us/dotnet/core/extensions/dependency-injection" target="_blank" data-v-664d0f7e>.NET dependency injection</a> as well as the <a href="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/options?view=aspnetcore-6.0" target="_blank" data-v-664d0f7e>options pattern</a>. This is a major departure from the component container adapter pattern used to date. Please take a look at the <a href="/shuttle-esb/upgrade.html" data-v-664d0f7e>upgrade guide</a> to see the <strong data-v-664d0f7e>breaking changes</strong>. </p><h1 class="tagline" data-v-664d0f7e>Flexible .Net queue and stream processing</h1><p class="description" data-v-664d0f7e> Configurable service bus that provides you with a mechanism to create cross-platform endpoints that are loosely coupled, enabling you to develop and deploy specific business functionality that can be independently versioned. </p><p class="actions" data-v-664d0f7e><a class="why" href="/shuttle-esb/concepts/why.html" data-v-664d0f7e>Why?</a><a class="get-started" href="/shuttle-esb/guide/introduction.html" data-v-664d0f7e> Get Started <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24" data-v-664d0f7e><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" data-v-664d0f7e></path></svg></a><a class="upgrade" href="/shuttle-esb/upgrade.html" data-v-664d0f7e>Upgrade</a></p>', 4);
+const _hoisted_6 = [
+  _hoisted_2
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("section", _hoisted_1, _hoisted_6);
+}
+var Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-664d0f7e"]]);
+const __pageData = '{"title":"Home","description":"","frontmatter":{"page":true,"title":"Home"},"headers":[],"relativePath":"index.md"}';
+const __default__ = {};
+const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", null, [
+        createVNode(Home)
+      ]);
+    };
+  }
+});
+export { __pageData, _sfc_main as default };
---
+++ assets/index.md.8027eb25.lean.js
@@ -0,0 +1,24 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode, e as createVNode } from "./app.28dce079.js";
+var Home_vue_vue_type_style_index_0_scoped_true_lang = "";
+const _sfc_main$1 = {};
+const _hoisted_1 = { id: "hero" };
+const _hoisted_2 = /* @__PURE__ */ createStaticVNode("", 4);
+const _hoisted_6 = [
+  _hoisted_2
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("section", _hoisted_1, _hoisted_6);
+}
+var Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-664d0f7e"]]);
+const __pageData = '{"title":"Home","description":"","frontmatter":{"page":true,"title":"Home"},"headers":[],"relativePath":"index.md"}';
+const __default__ = {};
+const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", null, [
+        createVNode(Home)
+      ]);
+    };
+  }
+});
+export { __pageData, _sfc_main as default };
